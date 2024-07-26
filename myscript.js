f5549dce426482b1859eee1b5400a7bc03fb109c
000danish000 <dhpinjari@outlook.com>
2024-07-26 12:21:38 +0530

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,20 +1,36 @@
 const newnote = document.getElementById('addnote');
 const maindiv = document.querySelector('.main');
 
-function createNewNote() {
+
+
+const saveNote = ()=>{
+    const notes = document.querySelectorAll('.notes #textarea');
+    const data = [];
+    notes.forEach((n)=>{
+        data.push(n.value);
+    })
+    localStorage.setItem("notes",JSON.stringify(data));
+}
+
+function createNewNote(text="") {
     const newnote = document.createElement('div');
     newnote.classList.add("notes");
     newnote.innerHTML = `<div class="toolbar">
                 <i class="fas fa-save"></i>
                 <i class="fas fa-trash"></i>
             </div>
-            <textarea name="textarea" id="textarea"></textarea>`;
+            <textarea name="textarea" id="textarea">${text}</textarea>`;
     maindiv.appendChild(newnote);
+
     newnote.querySelector('.fa-trash').addEventListener('click', () => {
         newnote.remove();
+        saveNote();
     })
     newnote.querySelector('.fa-save').addEventListener('click',()=>{
-
+        saveNote();
+    })
+    newnote.querySelector('#textarea').addEventListener('focusout',()=>{
+        saveNote();
     })
 }
 
@@ -23,6 +39,21 @@ newnote.addEventListener('click', () => {
     createNewNote();
 });
 
-const saveNote = ()=>{
+
+(
+    function () {
+        const lsnotes = JSON.parse(localStorage.getItem("notes"));
+        if(lsnotes == 0){
+            createNewNote();
+
+        }else{
+            lsnotes.forEach((ls) => {
+                createNewNote(ls);
+            })
     
-}
+
+        }
+    }
+)()
+
+
